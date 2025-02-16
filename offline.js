7bfc00ed4abb4559d4ce408deba0ce54947bd311
manofwar618 <manofwar618@gmail.com>
2025-02-15 19:17:58 -0500

    Add files via upload
    Version kikuto

--- offline.js
+++ offline.js
@@ -1,14 +1,15 @@
 ﻿{
-	"version": 1738480784,
+	"version": 1739664872,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
-		"jquery-2.1.1.min.js",
+		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/backgroundtile.png",
 		"images/player-sheet0.png",
 		"images/solidtile.png",
 		"images/jumpthrutile.png",
+		"images/sprite-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
